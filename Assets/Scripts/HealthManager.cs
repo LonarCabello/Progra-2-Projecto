21c7d3c7ea36ff545ee5d1957db4bf77962611f4
HoruslmL <fernandesorielilled@gmail.com>
2026-06-12 19:46:01 -0300

    Movimientos/Ataques Base del Jugador
    +Player Scripts/Models.
    +Weapons.

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    [Header("Vida")]
+    public int maxHealth = 150;
+
+    private int currentHealth;
+
+    [SerializeField] float damageCoolDown = 0.5f;
+    private bool canTakeDamage = true;  
+
+    public bool IsDead { get; private set; }
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();    
+        currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (IsDead)
+            return;
+
+        if (!canTakeDamage) 
+        {
+            return;
+        }
+
+        currentHealth -= damage;
+
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        anim.SetTrigger("TakenDamage");
+
+        Debug.Log(gameObject.name + " recibió " + damage + " de daño");
+
+        canTakeDamage = false;
+        Invoke(nameof(ResetDamageCoolDown), damageCoolDown);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        if (IsDead)
+            return;
+
+        currentHealth += amount;
+
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+    }
+
+    private void ResetDamageCoolDown()
+    {
+        canTakeDamage = true;
+    }
+
+    void Die()
+    {
+        IsDead = true;
+
+        anim.SetTrigger("Death");
+
+        Debug.Log(gameObject.name + " murió");
+    }
+
+    //Devuelve la vida actual
+
+    public int GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+
+    //Devuelve el porcentaje de la vida actual.
+
+    public float GetHealthPercent()
+    {
+        return (float)currentHealth / maxHealth;
+    }
+}
